194ff2c7eedd9427e000fd313bbe70ef6931a084
ckawoalt <ckawoalt@naver.com>
2023-02-08 16:06:57 +0900

    up to date

---
+++ models.py
@@ -0,0 +1,139 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from transformers import (RobertaConfig, RobertaModel, RobertaTokenizer,
+                          BartConfig, BartForConditionalGeneration, BartTokenizer,
+                          T5Config, T5ForConditionalGeneration, T5Tokenizer)
+import logging
+
+MODEL_CLASSES = {'roberta': (RobertaConfig, RobertaModel, RobertaTokenizer),
+                 't5': (T5Config, T5ForConditionalGeneration, T5Tokenizer),
+                 'codet5': (T5Config, T5ForConditionalGeneration, RobertaTokenizer),
+                 'bart': (BartConfig, BartForConditionalGeneration, BartTokenizer)}
+class Seq2Seq(nn.Module):
+    """
+        Build Seqence-to-Sequence.
+        Parameters:
+        * `encoder`- encoder of seq2seq model. e.g. roberta
+        * `decoder`- decoder of seq2seq model. e.g. transformer
+        * `config`- configuration of encoder model.
+        * `beam_size`- beam size for beam search.
+        * `max_length`- max length of target for beam search.
+        * `sos_id`- start of symbol ids in target for beam search.
+        * `eos_id`- end of symbol ids in target for beam search.
+    """
+
+    def __init__(self, encoder, decoder, config, beam_size=None, max_length=None, sos_id=None, eos_id=None):
+        super(Seq2Seq, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.config = config
+        self.register_buffer("bias", torch.tril(torch.ones(2048, 2048)))
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        self.lsm = nn.LogSoftmax(dim=-1)
+        self.tie_weights()
+
+        self.beam_size = beam_size
+        self.max_length = max_length
+        self.sos_id = sos_id
+        self.eos_id = eos_id
+
+    def _tie_or_clone_weights(self, first_module, second_module):
+        """ Tie or clone module weights depending of weither we are using TorchScript or not
+        """
+        if self.config.torchscript:
+            first_module.weight = nn.Parameter(second_module.weight.clone())
+        else:
+            first_module.weight = second_module.weight
+
+    def tie_weights(self):
+        """ Make sure we are sharing the input and output embeddings.
+            Export to TorchScript can't handle parameter sharing so we are cloning them instead.
+        """
+        self._tie_or_clone_weights(self.lm_head,
+                                   self.encoder.embeddings.word_embeddings)
+
+    def forward(self, source_ids=None, source_mask=None, target_ids=None, target_mask=None, args=None):
+        outputs = self.encoder(source_ids, attention_mask=source_mask)
+        encoder_output = outputs[0].permute([1, 0, 2]).contiguous()
+        if target_ids is not None:
+            attn_mask = -1e4 * (1 - self.bias[:target_ids.shape[1], :target_ids.shape[1]])
+            tgt_embeddings = self.encoder.embeddings(target_ids).permute([1, 0, 2]).contiguous()
+            out = self.decoder(tgt_embeddings, encoder_output, tgt_mask=attn_mask,
+                               memory_key_padding_mask=~source_mask)
+            # memory_key_padding_mask=(1 - source_mask).bool())
+            hidden_states = torch.tanh(self.dense(out)).permute([1, 0, 2]).contiguous()
+            lm_logits = self.lm_head(hidden_states)
+            # Shift so that tokens < n predict n
+            active_loss = target_mask[..., 1:].ne(0).view(-1) == 1
+            shift_logits = lm_logits[..., :-1, :].contiguous()
+            shift_labels = target_ids[..., 1:].contiguous()
+            # Flatten the tokens
+            loss_fct = nn.CrossEntropyLoss(ignore_index=-1)
+            loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1))[active_loss],
+                            shift_labels.view(-1)[active_loss])
+
+            outputs = loss, loss * active_loss.sum(), active_loss.sum()
+            return outputs
+        else:
+            # Predict
+            preds = []
+            zero = torch.cuda.LongTensor(1).fill_(0)
+            for i in range(source_ids.shape[0]):
+                context = encoder_output[:, i:i + 1]
+                context_mask = source_mask[i:i + 1, :]
+                beam = Beam(self.beam_size, self.sos_id, self.eos_id)
+                input_ids = beam.getCurrentState()
+                context = context.repeat(1, self.beam_size, 1)
+                context_mask = context_mask.repeat(self.beam_size, 1)
+                for _ in range(self.max_length):
+                    if beam.done():
+                        break
+                    attn_mask = -1e4 * (1 - self.bias[:input_ids.shape[1], :input_ids.shape[1]])
+                    tgt_embeddings = self.encoder.embeddings(input_ids).permute([1, 0, 2]).contiguous()
+                    out = self.decoder(tgt_embeddings, context, tgt_mask=attn_mask,
+                                       memory_key_padding_mask=~context_mask)
+                    # memory_key_padding_mask=(1 - context_mask).bool())
+                    out = torch.tanh(self.dense(out))
+                    hidden_states = out.permute([1, 0, 2]).contiguous()[:, -1, :]
+                    out = self.lsm(self.lm_head(hidden_states)).data
+                    beam.advance(out)
+                    input_ids.data.copy_(input_ids.data.index_select(0, beam.getCurrentOrigin()))
+                    input_ids = torch.cat((input_ids, beam.getCurrentState()), -1)
+                hyp = beam.getHyp(beam.getFinal())
+                pred = beam.buildTargetTokens(hyp)[:self.beam_size]
+                pred = [torch.cat([x.view(-1) for x in p] + [zero] * (self.max_length - len(p))).view(1, -1) for p in
+                        pred]
+                preds.append(torch.cat(pred, 0).unsqueeze(0))
+
+            preds = torch.cat(preds, 0)
+            return preds
+            
+def get_model_size(model):
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    model_size = sum([np.prod(p.size()) for p in model_parameters])
+    return "{}M".format(round(model_size / 1e+6))
+logger = logging.getLogger(__name__)
+
+def build_or_load_gen_model(args):
+    config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
+    config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path)
+    tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name)
+    if args.model_type == 'roberta':
+        encoder = model_class.from_pretrained(args.model_name_or_path, config=config)
+        decoder_layer = nn.TransformerDecoderLayer(d_model=config.hidden_size, nhead=config.num_attention_heads)
+        decoder = nn.TransformerDecoder(decoder_layer, num_layers=6)
+        model = Seq2Seq(encoder=encoder, decoder=decoder, config=config,
+                        beam_size=args.beam_size, max_length=args.max_target_length,
+                        sos_id=tokenizer.cls_token_id, eos_id=tokenizer.sep_token_id)
+    else:
+        model = model_class.from_pretrained(args.model_name_or_path)
+
+    logger.info("Finish loading model [%s] from %s", get_model_size(model), args.model_name_or_path)
+
+    if args.load_model_path is not None:
+        logger.info("Reload model from {}".format(args.load_model_path))
+        model.load_state_dict(torch.load(args.load_model_path))
+
+    return config, model, tokenizer
